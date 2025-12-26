506f381ca40c095f32ff59f36f836c20c93f72d2
blerner5 <blerner5@student.touro.edu>
2025-12-25 23:16:58 -0500

    extra closing braces here
    extra closing braces

--- exercises/src/main/java/hashmap_exercises/MinimumConsecutiveCards.java
+++ exercises/src/main/java/hashmap_exercises/MinimumConsecutiveCards.java
@@ -1,19 +1,9 @@
 package hashmap_exercises;
+import java.util.HashMap;
+import java.util.Map;
 
 
-/**
- * LeetCode 2260 - Minimum Consecutive Cards to Pick Up
- *
- * You are given an integer array cards where cards[i] represents the value of the i-th card.
- * You need to pick up a sequence of cards (a contiguous subarray) that contains at least
- * two cards with the same value.
- *
- * Return the minimum length of such a subarray, or -1 if it is impossible.
- *
- * Example:
- * cards = [3, 4, 2, 3, 4, 7] -> answer = 4
- * (e.g., subarray [3, 4, 2, 3] from index 0 to 3)
- */
+
 public class MinimumConsecutiveCards {
 
     /**
@@ -41,6 +31,5 @@ public class MinimumConsecutiveCards {
         // - For each index i:
         //     if cardValue was seen at j, update answer with (i - j + 1)
         //     then update lastIndexSeen for this cardValue to i
-        // - If you never update the answer, return -1
-    }
-}
+        // - If you never update the answer, return -1]]]
+    
